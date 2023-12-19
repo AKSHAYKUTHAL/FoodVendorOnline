306e744e5a02de1fd7aaece08420233d30dcc988
AKSHAYKUTHAL <akshaykuthal90@gmail.com>
2023-12-20 00:14:22 +0530

    login, reset password, email configuration

--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,12 @@ from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User,UserProfile
 from django.contrib import messages,auth
-from .utils import detectUser
+from .utils import detectUser, send_email_to_user
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+
 
 
 # Restrict the vendor from accessing the customer page
@@ -50,7 +53,13 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
-            messages.success(request,'Your account has been registered successfully')
+
+            # Send verification email
+            mail_subject = 'Please activate your food online account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_email_to_user(request, user, mail_subject, email_template)
+
+            messages.success(request,'Your account has been registered successfully.The activation link has been sent to the email. Please activate to use the account.')
             return redirect('accounts:registerUser')
         else:
             pass
@@ -88,7 +97,13 @@ def registerVendor(request):
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
-            messages.success(request,'Your account has been registered successfully! Please wait for the approval.')
+
+            # Send verification email
+            mail_subject = 'Please activate your food online account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_email_to_user(request, user, mail_subject, email_template)
+
+            messages.success(request,'Your account has been registered successfully! The activation link has been sent to the email. Please activate and wait for the approval.')
             return redirect('accounts:registerVendor')
         else:
             pass
@@ -101,6 +116,26 @@ def registerVendor(request):
         'v_form':v_form,
     }
     return render(request,'accounts/registerVendor.html',context)
+
+
+def activate(request, uidb64, token):
+    # Activate the user by setting the is_active status true
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'Your account has been activated successfully.')
+        return redirect('accounts:myAccount')
+    else:
+        messages.error(request,'Invalid activation link')
+        return redirect('accounts:myAccount')
+
+
 
 
 
@@ -150,3 +185,64 @@ def customerDashboard(request):
 @user_passes_test(check_role_vender)
 def vendorDashboard(request):
     return render(request,'accounts/vendorDashboard.html')
+
+
+
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+
+            # Send reset password email
+            mail_subject = 'Reset your password'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_email_to_user(request, user, mail_subject, email_template)
+
+            messages.success(request, 'The password reset link has been sent to your email adddress')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('accounts:forgot_password')
+
+    return render(request, 'accounts/forgot_password.html')
+
+
+
+
+def reset_password_validate(request, uidb64, token):
+    # Validate the user by decoding the token and user primary key
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, 'Please reset your password.')
+        return redirect('accounts:reset_password')
+    else:
+        messages.error(request,'This link has been expired')
+        return redirect('accounts:myAccount')
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password reset successfull')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('accounts:reset_password')
+    return render(request, 'accounts/reset_password.html')
--- food_vendor_main/settings.py
+++ food_vendor_main/settings.py
@@ -144,3 +144,14 @@ MESSAGE_TAGS = {
     messages.ERROR : 'danger',
     # messages.WARNING : 'warning',
 }
+
+
+
+# Email settings for sending emails through Gmail (example)
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') 
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
